dc3e9e2be0492ec66de8655bf2b5ac1ae3d11afd
Arif Burak Demiray <57103426+arifBurakDemiray@users.noreply.github.com>
2021-04-19 23:14:21 +0300

    refactor: new migration

---
+++ migrations/Migration20210419194842.ts
@@ -0,0 +1,11 @@
+import { Migration } from '@mikro-orm/migrations';
+
+export class Migration20210419194842 extends Migration {
+
+  async up(): Promise<void> {
+    this.addSql('alter table `thesis_topic_proposal` drop `status`;');
+
+    this.addSql('alter table `form_answer_field` drop index `form_answer_field_field_id_unique`;');
+  }
+
+}
